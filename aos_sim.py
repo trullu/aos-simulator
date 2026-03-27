decf3235f6ca7505f948301d2954c14837663a02
Davide Furlan <davide.furlan@mdhouse.it>
2026-03-27 14:13:58 +0100

    Forza Python 3.12 e template base

--- aos_sim.py
+++ aos_sim.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import uvicorn
 import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def home():
-    return {"message": "AoS Simulator funziona!"}
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
