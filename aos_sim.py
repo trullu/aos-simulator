2687b2a419ad8c0c1a660fb3969cdf6bebb5a213
Davide Furlan <davide.furlan@mdhouse.it>
2026-03-27 14:02:15 +0100

    Solo API JSON

--- aos_sim.py
+++ aos_sim.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 import uvicorn
 import os
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def home():
+    return {"message": "AoS Simulator funziona!"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
